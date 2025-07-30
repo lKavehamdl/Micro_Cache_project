7d0bac2388547ceadb43f82cadcc1a8d6ac2058e
lkavehamdl <kavehahmadi9090@gmail.com>
2025-07-30 22:32:38 +0330

    LRU Done!

--- Proj2.s
+++ Proj2.s
@@ -2,16 +2,17 @@
 .global _start
 .data
 	.balign 8
-	arr: .word 0x1, 0x11, 0x2, 0x4, 0x13, 0x4, 0x1
+	arr: .word 0x1, 0x5, 0x1, 0x9
 	cache: .word 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF 
 	@ 4 by 2 with 4 bytes per each block
+	LRUList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	FIFOList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	
 .text
 _start:
 	MOV R0, #0x0 @cache mode
 	MOV R2, #0x0 @current index 
-	MOV R3, #0x7 @input size
+	MOV R3, #0x4 @input size
 	MOV R4, #0x0 @hit count
 	MOV R5, #0x0 @miss count
 	B exec
@@ -52,8 +53,12 @@ FIFO:
 	B FIFO_miss
 	
 LRU:
-	ADD R2, R2, #0x1
-	BX LR
+	CMP R6, R1
+	BEQ LRU_first_hit
+	CMP R6, R8
+	BEQ LRU_second_hit
+	
+	B LRU_miss
 	
 	
 MRU:
@@ -75,46 +80,28 @@ RD:
 	ADD R2, R2, #0x1
 	BX LR
 	
-	
+
 FIFO_first_hit:
 	ADD R4, R4, #0x1 @hit detected
-	LDR R10, =FIFOList
-	AND R11, R6, #0x3 @find line
-	MOV R12, #0x4
-	MUL R11, R11, R12 @mul won't accept immediate
-	ADD R11, R11, R10 @index of FIFOList
-	LDR R12, [R11]
-	CMP R12, #0x0
-	MOVEQ R12, #0x1
-	STR R12, [R11]
 	ADD R2, R2, #0x1 @increment
 	BX LR
 	
 FIFO_second_hit:
 	ADD R4, R4, #0x1 @hit detected
-	LDR R10, =FIFOList
-	AND R11, R6, #0x3 @find line
-	MOV R12, #0x4
-	MUL R11, R11, R12 @mul won't accept immediate
-	ADD R11, R11, R10 @index of FIFOList
-	LDR R12, [R11]
-	CMP R12, #0x1
-	MOVEQ R12, #0x0
-	STR R12, [R11]
 	ADD R2, R2, #0x1 @increment
 	BX LR
 	
 FIFO_miss:
 	ADD R5, R5, #0x1 @miss detected
-	LDR R10, =FIFOList
+	LDR R10, =LRUList
 	AND R11, R6, #0x3 @find line
 	MOV R12, #0x4
 	MUL R11, R11, R12 @mul won't accept immediate
 	ADD R11, R11, R10 @index of FIFOList
 	LDR R12, [R11]
 	CMP R12, #0x0
-	BEQ FIFO_add_first_col
-	BNE FIFO_add_second_col
+	BEQ LRU_add_first_col
+	BNE LRU_add_second_col
 	
 FIFO_add_first_col:
 	ADD R12, R12, #0x1
@@ -129,6 +116,64 @@ FIFO_add_second_col:
 	STR R6, [R9] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
+
+	
+	
+LRU_first_hit:
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =LRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of FIFOList
+	LDR R12, [R11]
+	CMP R12, #0x0
+	MOVEQ R12, #0x1
+	STR R12, [R11]
+	ADD R2, R2, #0x1 @increment
+	BX LR
+	
+LRU_second_hit:
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =LRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of FIFOList
+	LDR R12, [R11]
+	CMP R12, #0x1
+	MOVEQ R12, #0x0
+	STR R12, [R11]
+	ADD R2, R2, #0x1 @increment
+	BX LR
+	
+LRU_miss:
+	ADD R5, R5, #0x1 @miss detected
+	LDR R10, =LRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of FIFOList
+	LDR R12, [R11]
+	CMP R12, #0x0
+	BEQ LRU_add_first_col
+	BNE LRU_add_second_col
+	
+LRU_add_first_col:
+	ADD R12, R12, #0x1
+	STR R12, [R11] @change First In for next round
+	STR R6, [R7] @store value in right position
+	ADD R2, R2, #0x1
+	BX LR
+
+LRU_add_second_col:
+	SUB R12, R12, #0x1
+	STR R12, [R11] @change First In for next round
+	STR R6, [R9] @store value in right position
+	ADD R2, R2, #0x1
+	BX LR
+
+
 
 _END:
 	B _END
