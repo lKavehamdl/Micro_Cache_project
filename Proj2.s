ee85a212c0709ce835da06ce50c203f7d590915d
lkavehamdl <kavehahmadi9090@gmail.com>
2025-07-30 23:04:14 +0330

    MRU Done!

--- Proj2.s
+++ Proj2.s
@@ -5,12 +5,13 @@
 	arr: .word 0x1, 0x5, 0x1, 0x9
 	cache: .word 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF 
 	@ 4 by 2 with 4 bytes per each block
-	LRUList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	FIFOList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
+	LRUList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
+	MRUList: .word 0x1, 0x1, 0x1, 0x1 @one per each line
 	
 .text
 _start:
-	MOV R0, #0x0 @cache mode
+	MOV R0, #0x2 @cache mode
 	MOV R2, #0x0 @current index 
 	MOV R3, #0x4 @input size
 	MOV R4, #0x0 @hit count
@@ -62,8 +63,12 @@ LRU:
 	
 	
 MRU:
-	ADD R2, R2, #0x1
-	BX LR
+	CMP R6, R1
+	BEQ MRU_first_hit
+	CMP R6, R8
+	BEQ MRU_second_hit
+	
+	B MRU_miss
 	
 	
 LFU:
@@ -125,7 +130,7 @@ LRU_first_hit:
 	AND R11, R6, #0x3 @find line
 	MOV R12, #0x4
 	MUL R11, R11, R12 @mul won't accept immediate
-	ADD R11, R11, R10 @index of FIFOList
+	ADD R11, R11, R10 @index of LRUList
 	LDR R12, [R11]
 	CMP R12, #0x0
 	MOVEQ R12, #0x1
@@ -139,7 +144,7 @@ LRU_second_hit:
 	AND R11, R6, #0x3 @find line
 	MOV R12, #0x4
 	MUL R11, R11, R12 @mul won't accept immediate
-	ADD R11, R11, R10 @index of FIFOList
+	ADD R11, R11, R10 @index of LRUList
 	LDR R12, [R11]
 	CMP R12, #0x1
 	MOVEQ R12, #0x0
@@ -153,7 +158,7 @@ LRU_miss:
 	AND R11, R6, #0x3 @find line
 	MOV R12, #0x4
 	MUL R11, R11, R12 @mul won't accept immediate
-	ADD R11, R11, R10 @index of FIFOList
+	ADD R11, R11, R10 @index of LRUList
 	LDR R12, [R11]
 	CMP R12, #0x0
 	BEQ LRU_add_first_col
@@ -161,18 +166,73 @@ LRU_miss:
 	
 LRU_add_first_col:
 	ADD R12, R12, #0x1
-	STR R12, [R11] @change First In for next round
+	STR R12, [R11] @change replacement index for next round
 	STR R6, [R7] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
 
 LRU_add_second_col:
 	SUB R12, R12, #0x1
-	STR R12, [R11] @change First In for next round
+	STR R12, [R11] @change replacement index for next round
 	STR R6, [R9] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
 
+
+
+MRU_first_hit: 
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =MRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of MRUList
+	MOV R12, #0x0
+	STR R12, [R11]
+	ADD R2, R2, #0x1 @increment
+	BX LR
+	
+MRU_second_hit: 
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =MRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of MRUList
+	MOV R12, #0x1
+	STR R12, [R11]
+	ADD R2, R2, #0x1 @increment
+	BX LR
+	
+MRU_miss: @TODO
+	ADD R5, R5, #0x1 @miss detected
+	CMP R1, #0xFFFFFFFF @-1 means cache is empty, no replacement required
+	BEQ MRU_add_first_col
+	CMP R8, #0xFFFFFFFF @-1 means cache is empty, no replacement required
+	BEQ MRU_add_second_col
+	LDR R10, =MRUList
+	AND R11, R6, #0x3 @find line
+	MOV R12, #0x4
+	MUL R11, R11, R12 @mul won't accept immediate
+	ADD R11, R11, R10 @index of MRUList
+	LDR R12, [R11]
+	CMP R12, #0x0
+	BEQ MRU_add_first_col
+	BNE MRU_add_second_col
+	
+MRU_add_first_col: 
+	MOV R12, #0x0
+	STR R12, [R11] @change replacement index for next round
+	STR R6, [R7] @store value in right position
+	ADD R2, R2, #0x1
+	BX LR
+
+MRU_add_second_col: 
+	MOV R12, #0x1
+	STR R12, [R11] @change replacement index for next round
+	STR R6, [R9] @store value in right position
+	ADD R2, R2, #0x1
+	BX LR
 
 
 _END:
