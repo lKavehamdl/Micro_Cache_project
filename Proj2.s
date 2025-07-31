9daa5496b4c0c2bb0cab1703f17b2eb4c77b0877
lkavehamdl <kavehahmadi9090@gmail.com>
2025-07-31 10:38:38 +0330

    LFU Done!

--- Proj2.s
+++ Proj2.s
@@ -1,17 +1,18 @@
 @KOMAK!
 .global _start
 .data
-	.balign 8
+	.balign 32
 	arr: .word 0x1, 0x5, 0x1, 0x9
 	cache: .word 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF 
 	@ 4 by 2 with 4 bytes per each block
 	FIFOList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	LRUList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	MRUList: .word 0x1, 0x1, 0x1, 0x1 @one per each line
+	FrequencyList: .word 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 @per each block of cache 
 	
 .text
 _start:
-	MOV R0, #0x2 @cache mode
+	MOV R0, #0x3 @cache mode
 	MOV R2, #0x0 @current index 
 	MOV R3, #0x4 @input size
 	MOV R4, #0x0 @hit count
@@ -24,13 +25,12 @@ exec:
 	CMP R3, R2
 	BEQ _END
 	LDR R6, [R1, R2, LSL #0x2] @current val
-	AND R7, R6, #3 @find the line
+	AND R7, R6, #0x3 @find the line
 	MOV R12, #0x8
 	MUL R7, R7, R12 @mul won't accept immediate
-	ADD R7, R7, R8 @first col of given row
-	LDR R1, [R7]
+	LDR R1, [R7, R8]
 	ADD R9, R7, #0x4 @second col of given row
-	LDR R8, [R9]
+	LDR R8, [R9, R8]
 	CMP R0, #0x0
 	BLEQ FIFO
 	CMP R0, #0x1
@@ -72,8 +72,12 @@ MRU:
 	
 	
 LFU:
-	ADD R2, R2, #0x1
-	BX LR
+	CMP R6, R1
+	BEQ LFU_first_hit
+	CMP R6, R8
+	BEQ LFU_second_hit
+	
+	B LFU_miss
 	
 	
 MFU:
@@ -98,7 +102,7 @@ FIFO_second_hit:
 	
 FIFO_miss:
 	ADD R5, R5, #0x1 @miss detected
-	LDR R10, =LRUList
+	LDR R10, =FIFOList
 	AND R11, R6, #0x3 @find line
 	MOV R12, #0x4
 	MUL R11, R11, R12 @mul won't accept immediate
@@ -109,14 +113,14 @@ FIFO_miss:
 	BNE LRU_add_second_col
 	
 FIFO_add_first_col:
-	ADD R12, R12, #0x1
+	MOV R12, #0x1
 	STR R12, [R11] @change First In for next round
 	STR R6, [R7] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
 
 FIFO_add_second_col:
-	SUB R12, R12, #0x1
+	MOV R12, #0x0
 	STR R12, [R11] @change First In for next round
 	STR R6, [R9] @store value in right position
 	ADD R2, R2, #0x1
@@ -165,14 +169,14 @@ LRU_miss:
 	BNE LRU_add_second_col
 	
 LRU_add_first_col:
-	ADD R12, R12, #0x1
+	MOV R12, #0x1
 	STR R12, [R11] @change replacement index for next round
 	STR R6, [R7] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
 
 LRU_add_second_col:
-	SUB R12, R12, #0x1
+	MOV R12, #0x0
 	STR R12, [R11] @change replacement index for next round
 	STR R6, [R9] @store value in right position
 	ADD R2, R2, #0x1
@@ -204,7 +208,7 @@ MRU_second_hit:
 	ADD R2, R2, #0x1 @increment
 	BX LR
 	
-MRU_miss: @TODO
+MRU_miss: 
 	ADD R5, R5, #0x1 @miss detected
 	CMP R1, #0xFFFFFFFF @-1 means cache is empty, no replacement required
 	BEQ MRU_add_first_col
@@ -233,6 +237,53 @@ MRU_add_second_col:
 	STR R6, [R9] @store value in right position
 	ADD R2, R2, #0x1
 	BX LR
+	
+LFU_first_hit:
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =FrequencyList
+	LDR R11, [R10, R7] @index of Frequency List
+	ADD R11, R11, #0x1
+	STR R11, [R10, R7] @store new value of frequency for this
+	ADD R2, R2, #0x1
+	BX LR
+
+LFU_second_hit:
+	ADD R4, R4, #0x1 @hit detected
+	LDR R10, =FrequencyList
+	LDR R11, [R10, R9] @index of Frequency List
+	ADD R11, R11, #0x1
+	STR R12, [R10, R9] @store new value of frequency for this
+	ADD R2, R2, #0x1
+	BX LR
+
+
+LFU_miss:
+	ADD R5, R5, #0x1 @miss detected
+	LDR R10, =FrequencyList
+	LDR R8, [R9, R10] @value of second column in freq list
+	LDR R1, [R7, R10] @value of first columb in freq list
+	CMP R1, R8
+	BLE LFU_add_first_col
+	B LFU_add_second_col
+	
+
+LFU_add_first_col:
+	MOV R1, #0x1
+	STR R1, [R7, R10]
+	LDR R8, =cache
+	STR R6, [R7, R8]
+	ADD R2, R2, #0x1
+	BX LR
+
+LFU_add_second_col:
+	MOV R1, #0x1
+	STR R1, [R9, R10]
+	LDR R8, =cache
+	STR R6, [R9, R8]
+	ADD R2, R2, #0x1
+	BX LR
+	
+	
 
 
 _END:
