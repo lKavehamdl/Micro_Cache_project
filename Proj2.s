674c2f753cb8f6bd38cef8d726a05ff2396e3d11
lkavehamdl <kavehahmadi9090@gmail.com>
2025-07-31 11:20:23 +0330

    Project Done!(without bonus)

--- Proj2.s
+++ Proj2.s
@@ -9,10 +9,11 @@
 	LRUList: .word 0x0, 0x0, 0x0, 0x0 @one per each line
 	MRUList: .word 0x1, 0x1, 0x1, 0x1 @one per each line
 	FrequencyList: .word 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 @per each block of cache 
+	RandomList: .word 0xABD234FE, 0x1234FDEF, 0xDDCC2345, 0xACDF6543 @size of sequence  
 	
 .text
 _start:
-	MOV R0, #0x4 @cache mode
+	MOV R0, #0x5 @cache mode
 	MOV R2, #0x0 @current index 
 	MOV R3, #0x4 @input size
 	MOV R4, #0x0 @hit count
@@ -90,8 +91,12 @@ MFU:
 	
 	
 RD:
-	ADD R2, R2, #0x1
-	BX LR
+	CMP R6, R1
+	BEQ RD_first_hit
+	CMP R6, R8
+	BEQ RD_second_hit
+	
+	B RD_miss
 	
 
 FIFO_first_hit:
@@ -336,7 +341,42 @@ MFU_add_second_col:
 	STR R6, [R9, R8]
 	ADD R2, R2, #0x1
 	BX LR
+	
+RD_first_hit:
+	ADD R4, R4, #0x1 @hit detected
+	ADD R2, R2, #0x1
+	BX LR
 
+RD_second_hit:
+	ADD R4, R4, #0x1 @hit detected
+	ADD R2, R2, #0x1
+	BX LR
+
+RD_miss:
+	ADD R5, R5, #0x1 @miss detected
+	LDR R10, =RandomList
+	CMP R1, #0xFFFFFFFF @-1 means cache is empty, no repleacment reqiured
+	BEQ RD_add_first_col
+ 	CMP R8, #0xFFFFFFFF @-1 means cache is empty, no repleacment reqiured
+	BEQ RD_add_second_col
+	LDR R11, [R10, R2, LSL #0x2]
+	AND R11, R11, #0x1
+	CMP R11, #0x0
+	BEQ RD_add_first_col
+	B RD_add_second_col
+	
+RD_add_first_col:
+	LDR R8, =cache
+	STR R6, [R7, R8]
+	ADD R2, R2, #0x1
+	BX LR
+
+
+RD_add_second_col:
+	LDR R8, =cache
+	STR R6, [R9, R8]
+	ADD R2, R2, #0x1
+	BX LR
 
 
 _END:
